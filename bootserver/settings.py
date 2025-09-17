b7e6467bcae0f16f9dee1bbf5fe9bb9684c71003
abdennour_laoubi <108241975+ABDELNOURLA@users.noreply.github.com>
2025-09-17 13:05:43 +0200

    Update settings.py
    little changes

--- bootserver/settings.py
+++ bootserver/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@r&k6_=k^_24!dtwozli9v3#es0x7r_p3mra9owal%ki2ykb3x'
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY","fallback-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
